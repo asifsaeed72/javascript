e623023d43057a243d3fa9c0f63d13e413c6266a
Rana Asif Saeed <ranaasifsaeed123@gmail.com>
2024-06-14 11:34:25 +0000

    datatype

---
+++ DataTypes/types.js
@@ -0,0 +1,14 @@
+let name= " Asif saeed";
+let age = 22;
+let city= "Lahore"
+//console.log([name,age,city])
+
+
+// ------ Data Types -----
+// number    1, 22
+// String     Asif
+//boolean  true,false
+// null    Standalone value (subject)
+// Undefined     ==> undefined
+
+console.log(typeof  null)
