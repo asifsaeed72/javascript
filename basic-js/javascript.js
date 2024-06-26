0361a7157410e84e000c00995190b7228873e7cb
Rana Asif Saeed <ranaasifsaeed123@gmail.com>
2024-06-26 21:03:15 +0500

    data type

--- basic-js/javascript.js
+++ basic-js/javascript.js
@@ -3,13 +3,12 @@ console.log(" Today is Friday")
 <<<<<<< HEAD
 let name="Asif Saeed"
 const id = 123
-=======
+// =======
 let name="Asif Saeed"
 console.log(name)
->>>>>>> 7cba87f863a20243da622a54863fd96ceff1d05c
 
-<<<<<<< HEAD
+// <<<<<<< HEAD
 console.log([name,id])
-=======
+// =======
 
->>>>>>> 7cba87f863a20243da622a54863fd96ceff1d05c
+console.log( typeof name)
