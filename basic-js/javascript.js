13d83b27110bf8b1aaab0f4edfe05a33fe9cca49
Rana Asif Saeed <ranaasifsaeed123@gmail.com>
2024-06-26 17:52:59 +0500

    add

--- basic-js/javascript.js
+++ basic-js/javascript.js
@@ -1,5 +1,15 @@
 console.log(" hey asif kisy ho")
 console.log(" Today is Friday")
+<<<<<<< HEAD
+let name="Asif Saeed"
+const id = 123
+=======
 let name="Asif Saeed"
 console.log(name)
+>>>>>>> 7cba87f863a20243da622a54863fd96ceff1d05c
 
+<<<<<<< HEAD
+console.log([name,id])
+=======
+
+>>>>>>> 7cba87f863a20243da622a54863fd96ceff1d05c
