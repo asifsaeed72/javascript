0161f7b74eedc1f2c9d97746bb847237e91c30be
Rana Asif Saeed <ranaasifsaeed123@gmail.com>
2024-08-26 12:03:16 +0000

    String

--- DataTypes/String.js
+++ DataTypes/String.js
@@ -1,3 +1,12 @@
 a = "asif"
 b = " saeed"
-console.log(`my nam eis ${a} and my father name is ${b}`)
+//console.log(`my nam eis ${a} and my father name is ${b}`)
+const gameName = new String("GTA vice city")
+// console.log(gameName)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('t'))
+const url = "http://asifsaeed.com/asif20%saeed"
+console.log(url.replace('20%', '_'))
+console.log(url.split(','))
+
---
+++ DataTypes/number.js
@@ -0,0 +1,25 @@
+ const balance = new Number(400)
+ // console.log(balance.toString().length)
+ const score = 400
+ console.log(score)
+ const hundred = 1000000 
+ // console.log(hundred.toLocaleString())
+
+ // -------------------------------  Math ==================================
+
+ // console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4,6))
+
+const min = 10
+const max = 20
+// console.log(Math.floor(Math.random() * (max - -min) *1))
+
+
+// =================================================== DATE ==============================================
+let date = new Date()
+console.log(date.toDateString())
+console.log(date.toString())
+
+let myDate = new Date(2024,0,24)
+console.log(myDate)
