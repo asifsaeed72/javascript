8045b4585a3cdb14b671b9daacc64b33ab37fb07
Rana Asif Saeed <ranaasifsaeed123@gmail.com>
2024-08-26 14:14:04 +0000

    add

---
+++ DataTypes/Array.js
@@ -0,0 +1,13 @@
+const myArr = [1,2,3,4,5]
+// console.log(myArr[1])
+// const arr = new Array(7,8,9,0)
+// console.log(arr.length())
+// +++++++++++++++++++++++++++++++++++++++++ Array Methode +++++++++++++++++++++++++++++++++++++
+myArr.push(6)
+// console.log(myArr)
+myArr.pop()
+// console.log(myArr)
+myArr.unshift(9)
+// console.log(myArr)
+myArr.shift()
+// console.log(myArr)
